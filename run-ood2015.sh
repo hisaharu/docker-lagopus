6aca49441c0d21e2049a97c2aa33bd5b2702079b
Hisaharu Ishii <hisaharu@gmail.com>
2015-12-15 17:14:11 +0900

    use pipe to reduce disk I/O

--- run-ood2015.sh
+++ run-ood2015.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://10.10.2.252/docker-lagopus.tar
-sudo docker load -i docker-lagopus.tar
+sudo apt-get install -y curl
+curl http://10.10.2.252/docker-lagopus.tar | sudo docker load
 sudo docker run -itd --name lagopus --net host --privileged hisaharu/docker-lagopus
 
